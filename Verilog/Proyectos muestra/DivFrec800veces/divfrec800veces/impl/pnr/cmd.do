65ca310a05386228c486e23f47e8f2ac2446922a
ggc1509 <ggc1509@gmail.com>
2026-09-17 00:36:32 -0600

    Add files via upload

---
+++ Verilog/Proyectos muestra/DivFrec800veces/divfrec800veces/impl/pnr/cmd.do
@@ -0,0 +1,17 @@
+-d C:\Users\ger_1\OneDrive\Documentos\Verilog\ProySemana2\DivFrec800veces\divfrec800veces\impl\gwsynthesis\divfrec800veces.vg
+-p GW1NR-9C-QFN88P-6
+-pn GW1NR-LV9QN88PC6/I5
+-cst C:\Users\ger_1\OneDrive\Documentos\Verilog\ProySemana2\DivFrec800veces\divfrec800veces\src\divfrec800veces.cst
+-cfg C:\Users\ger_1\OneDrive\Documentos\Verilog\ProySemana2\DivFrec800veces\divfrec800veces\impl\pnr\device.cfg
+-bit
+-tr
+-ph
+-timing
+-cst_error
+-convert_sdp32_36_to_sdp16_18
+-place_option 0
+-route_option 0
+-clock_route_order 0
+-correct_hold 1
+-route_maxfan 23
+-global_freq 50.000
---
+++ Verilog/Proyectos muestra/Mux4a1/src/Mux4a1.v
@@ -0,0 +1,33 @@
+/*
+    Multiplexor 4 a 1
+    Este proyecto consiste de un multiplexor de 4 bits a la entrada 
+    y una linea de salida. Para seleccionar la linea de entrada se 
+    utilizan dos bits.
+                 _______________
+                |               |
+   entrada0 --->|               |---> Salida
+   entrada1 --->|               |
+   entrada2 --->|               |
+   entrada3 --->|               |
+                |               |
+   Selector1--->|               |
+   Selector2--->|               |
+                |_______________|     */
+
+
+module mux41c (
+    input wire [3:0] entrada ,
+    input wire [1:0] selector ,
+    output reg salida
+);
+always @(*)
+    case(selector)
+        0: salida = entrada[0];
+        1: salida = entrada[1];
+        2: salida = entrada[2];
+        3: salida = entrada[3];
+        default: salida = entrada[0];
+    endcase
+endmodule
+
+
